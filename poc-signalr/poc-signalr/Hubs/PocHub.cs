be934c9105a02267f2a8604092211f4287e9c07b
jromansah <jromansah@gmail.com>
2022-02-03 22:23:21 +0100

    Initial commit

---
+++ poc-signalr/poc-signalr/Hubs/PocHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.AspNetCore.SignalR;
+using poc_signalr.Interfaces;
+
+namespace poc_signalr.Hubs
+{
+    public class PocHub : Hub<IPocHub>
+    {
+        public PocHub()
+        {
+        }
+
+        public void GetMessage()
+        {
+            Clients.Caller.NotifyConnection("Connection with Pochub succeed!");
+        }
+
+        public void GetCounter() {
+            for (int i = 0; i < 50; i++)
+            {
+                Thread.Sleep(1000);
+                Clients.Caller.Counter(i);
+            }
+        }
+    }
+}
---
+++ poc-signalr/poc-signalr/Interfaces/IPocHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace poc_signalr.Interfaces
+{
+    public interface IPocHub
+    {
+        Task NotifyConnection(string message);
+        Task Counter(int number);
+    }
+}
